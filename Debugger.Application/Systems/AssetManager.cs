4e729fba841bb40950b37dc0c851196a8bef27fd
jastcher <hugo.foltin@protonmail.com>
2026-07-09 16:51:23 +0200

    added font, white pixel, UI style

--- Debugger.Application/Systems/AssetManager.cs
+++ Debugger.Application/Systems/AssetManager.cs
@@ -1,22 +1,37 @@
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using MLEM.Font;
+using MLEM.Ui.Style;
 
 namespace Debugger.Application.Systems
 {
-
-
     public static class AssetManager
     {
         private static readonly Dictionary<string, Texture2D> _textures = new();
-        
-        public static SpriteFont Font {get;private set;}
 
-        public static void LoadAll(ContentManager content)
+        public static SpriteFont Font { get; private set; }
+
+        public static Texture2D WhitePixel { get; private set; }
+
+        public static UiStyle DefaultUiStyle { get; private set; }
+        public static void LoadAll(ContentManager content, GraphicsDevice graphicsDevice)
         {
             _textures["player"] = content.Load<Texture2D>("Sprites/arrow");
-            
+
             Font = content.Load<SpriteFont>("Font");
+
+            WhitePixel = new Texture2D(graphicsDevice, 1, 1);
+            Color[] colorData = [Color.White];
+            WhitePixel.SetData(colorData);
+
+            DefaultUiStyle = new UntexturedStyle(new SpriteBatch(graphicsDevice))
+            {
+                Font = new GenericSpriteFont(Font),
+                //TextColor = Color.Red
+            };
+
         }
 
         public static Texture2D Get(string key)
